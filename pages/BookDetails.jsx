d0ef4fc375aa4a5747be79b5f286cbff03d9a1b4
Konstantin Nochevny <kosnobiz@gmail.com>
2023-12-10 21:38:26 +0200

    Modify render BookDetails to show relevant info

--- pages/BookDetails.jsx
+++ pages/BookDetails.jsx
@@ -15,9 +15,22 @@ export function BookDetails({ bookId, onBack }) {
     if (!book) return <div>Loading...</div>
     return (
         <section className="book-details">
-            <h3>Book Vendor: {book.vendor}</h3>
-            <h3>Book Speed: {book.maxSpeed}</h3>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis quae fuga eveniet, quisquam ducimus modi optio in alias accusantium corrupti veritatis commodi tenetur voluptate deserunt nihil quibusdam. Expedita, architecto omnis?</p>
+
+            <img className="book-image" src={book.thumbnail} />
+
+            <p className="book-categories">Categories: {book.categories.join(", ")}</p>
+            <h3 className="book-title">{book.title}</h3>
+            <p className="book-sub-title">By {book.subtitle}</p>
+            <p className="book-author">By {book.authors}</p>
+
+
+            <div className="book-stats">
+            <p className="book-price">Price: {book.listPrice.amount}{book.listPrice.currencyCode}</p>
+                <p className="book-publish-date">Published at: {book.publishedDate}</p>
+                <p className="book-page-count">Pages: {book.pageCount}</p>
+                <p className="book-language">Language: {book.language}</p>
+                <p className="book-id">ID: {book.id}</p>
+            </div>
             <button onClick={onBack}>Back</button>
         </section>
     )
