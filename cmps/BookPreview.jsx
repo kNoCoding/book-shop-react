8bf9d7be2d0845568d02019851a3a5a61abd0b44
Konstantin Nochevny <kosnobiz@gmail.com>
2023-12-10 21:04:52 +0200

    Modify render BookPreview to show relevant info

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -3,9 +3,12 @@
 export function BookPreview({ book }) {
     return (
         <article className="book-preview">
-            <img src={book.thumbnail} />
+            <img className="book-image" src={book.thumbnail} />
             <h3 className="book-title">{book.title}</h3>
-            <h4>Book Speed {book.maxSpeed}</h4>
+            <p className="book-author">By {book.authors}</p>
+
+            <p className="book-price">{book.listPrice.amount}{book.listPrice.currencyCode}</p>
+            {/* <button className="book-buy-button">Buy Now!</button> */}
         </article>
     )
 }
